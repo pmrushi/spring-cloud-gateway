668070d79bde97c3c184321a198dae5363e85bd0
Rushi Pamu <rushi.p@imaginea.com>
2021-05-20 22:10:57 +0530

    Added java file based cloud gateway config.

---
+++ api-gateway/src/main/java/com/example/gateway/SpringCloudConfig.java
@@ -0,0 +1,20 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/orders/**")
+                        .uri("http://localhost:9001/"))
+                .route(r -> r.path("/api/v1/products/**")
+                        .uri("http://localhost:9002/"))
+                .build();
+    }
+}
